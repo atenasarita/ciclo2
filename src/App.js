9704eef7d4f8c00db4fdf85d0751b3e175a68486
Atenas Arita <150080590+atenasarita@users.noreply.github.com>
2025-05-31 18:39:38 -0600

    Update routes in App.js
    Routes para nuevas páginas (ForgotPassword, VerifyOTP, Reset-Password)

--- src/App.js
+++ src/App.js
@@ -2,29 +2,36 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainPage           from './pages/MainPage';
-import Login          from './pages/Login';
-import SignUp         from './pages/Signup';
-import Dashboard      from './pages/Dashboard';
-import SessionStarted from './pages/SessionStarted';
-import Admin          from './pages/Admin';
-import Account        from './pages/Account';
-import LogWaste        from './pages/LogWaste';
+import Login              from './pages/Login';
+import SignUp             from './pages/Signup';
+import Dashboard          from './pages/Dashboard';
+import SessionStarted     from './pages/SessionStarted';
+import Admin              from './pages/Admin';
+import Account            from './pages/Account';
+import LogWaste           from './pages/LogWaste';
 import HistoryView        from './pages/HistoryView';
+import ForgotPassword     from './pages/ForgotPassword';
+import VerifyOTP          from './pages/VerifyOTP';
+import ResetPassword      from './pages/ResetPassword';
+
 import '../src/App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"           element={<MainPage />} />
-        <Route path="/login"      element={<Login />} />
-        <Route path="/signup"     element={<SignUp />} />
-        <Route path="/dashboard"  element={<Dashboard />} />
-        <Route path="/sessionstarted" element={<SessionStarted />} />
-        <Route path="/admin"      element={<Admin />} />
-        <Route path="/account"    element={<Account />} />
-        <Route path="/logwaste"    element={<LogWaste />} />
-        <Route path="/historyview"    element={<HistoryView />} />
+        <Route path="/"                  element={<MainPage />} />
+        <Route path="/login"             element={<Login />} />
+        <Route path="/signup"            element={<SignUp />} />
+        <Route path="/dashboard"         element={<Dashboard />} />
+        <Route path="/sessionstarted"    element={<SessionStarted />} />
+        <Route path="/admin"             element={<Admin />} />
+        <Route path="/account"           element={<Account />} />
+        <Route path="/logwaste"          element={<LogWaste />} />
+        <Route path="/historyview"       element={<HistoryView />} />
+        <Route path="/forgot-password"      element={<ForgotPassword />} />
+        <Route path="/verify-otp"           element={<VerifyOTP />} />
+        <Route path="/reset-password"       element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
