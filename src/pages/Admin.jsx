75c8d27bf9840a2c68660bfb52ad102f951f75b4
Atenas Arita <150080590+atenasarita@users.noreply.github.com>
2025-05-31 18:31:10 -0600

    Update Admin.jsx - User Approval Cards
    Conexión a los endpoints necesarios en back.js

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,15 +1,51 @@
 import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import ApprovalCard from '../components/ApprovalCard';
 import '../styles/admin-styles.css';
 
-const dummyUsers = [
-  { name: 'Jane Doe', username: 'N00949600', email: 'jane@kia.com' },
-  { name: 'John Doe', username: 'N01331090', email: 'john@kia.com' },
-];
-
 export default function Admin() {
-  const handleApprove = user => {};
-  const handleReject  = user => {};
+  const [noAprobados, setNoAprobados] = useState([]);
+
+  const fetchNoAprobados = async () => {
+    const noAprobadosRes = await fetch('http://localhost:3001/no_aprobados');
+    const noAprobadosData = await noAprobadosRes.json();
+    setNoAprobados(noAprobadosData);
+  };
+
+  useEffect(() => {
+    fetchNoAprobados();
+  }, []);
+
+  
+  
+  const handleApprove = async (user) => {
+    try {
+      await fetch(`http://localhost:3001/approve_empleado/${user}`,{
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: user })
+      });
+      
+      fetchNoAprobados(); // actualiza la lista
+    } catch (err) {
+      console.error('Error aprobando usuario:', err);
+    }
+  };
+  
+  const handleReject = async (user) => {
+    try {
+      await fetch(`http://localhost:3001/reject_empleado/${user}`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id: user })
+      });
+
+      console.log(user);
+      fetchNoAprobados(); // actualiza la lista
+    } catch (err) {
+      console.error('Error rechazando usuario:', err);
+    }
+  };
 
   return (
     <div className='admin-page'>
@@ -27,7 +63,7 @@ export default function Admin() {
           <h1>Pending Sign Ups Requests</h1>
         </div>
         <div className="approval-list">
-          {dummyUsers.map(u => (
+          {noAprobados.map(u => (
             <ApprovalCard key={u.username} user={u}
               onApprove={handleApprove} onReject={handleReject} />
           ))}
